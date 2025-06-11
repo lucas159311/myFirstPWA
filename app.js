e43ae96102f5d346777cefd8e88eb2773da8ab16
Lucas Avelino <50250488+lucas159311@users.noreply.github.com>
2025-06-11 11:46:56 -0300

    Removendo  o location.pathname do service worker

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function() {
-        navigator.serviceWorker.register(`${location.pathname}service-worker.js`)
+        navigator.serviceWorker.register(`./service-worker.js`)
     });
 }
