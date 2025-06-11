e77d667058214e348d304bec4d9ef69683871a2c
Lucas Avelino <50250488+lucas159311@users.noreply.github.com>
2025-06-11 11:30:52 -0300

    testando novo service worker

---
+++ service-worker.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = "meu-pwa-cache-v1";
+const urlsToCache = ["/", "/index.html", "/style.css", "/app.js", "/offline.html"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
